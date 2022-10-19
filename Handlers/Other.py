5c3f021a750e149be1a23e698d0ed986fa3a3fdb
Mac <avtolandea@gmail.com>
2022-10-19 20:48:21 +0300

    english

--- Handlers/Other.py
+++ Handlers/Other.py
@@ -30,7 +30,8 @@ async def spam_start():
 
 
 async def english_spam_start():
-    aioschedule.every().day.at('17:45').do(english)
+    aioschedule.every().day.at('17:52').do(english)
+
     while True:
         await aioschedule.run_pending()
         await asyncio.sleep(0)
